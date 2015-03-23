f73de247d2cd1315af1f446d5ef02450f636dc40
Matthew Rudy Jacobs <matthewrudyjacobs@gmail.com>
2015-03-23 15:16:27 +0800

    disable spec monkey patching

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,6 +1,10 @@
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'resume'
 
+RSpec.configure do |c|
+  c.disable_monkey_patching!
+end
+
 RSpec::Matchers.define :be_present do
   match do |actual|
     !actual.nil? && actual.length > 0
